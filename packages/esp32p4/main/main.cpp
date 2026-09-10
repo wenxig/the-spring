3aa4ce35be30dfcfa6365addef1d2d9b7b3f25f0
wenxig <wenxinguo12@gmail.com>
2026-09-10 13:26:36 +0800

    feat(esp32p4): 接入按键交互与休眠路由

--- packages/esp32p4/main/main.cpp
+++ packages/esp32p4/main/main.cpp
@@ -12,6 +12,25 @@
 #include "ota_service.hpp"
 #include "wifi_service.hpp"
 #include "power_service.hpp"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+namespace {
+void interaction_task(void*) {
+  while (true) {
+    spring::input::Key key{};
+    if (!spring::input::next(key, 1000)) continue;
+    if (key == spring::input::Key::home) spring::app::navigate_home();
+    if (key == spring::input::Key::sleep) {
+      if (spring::power::state() == spring::power::State::sleeping) {
+        spring::power::wake();
+      } else {
+        spring::power::request_sleep();
+      }
+    }
+  }
+}
+}
 
 namespace {
 constexpr char kTag[] = "the_spring";
@@ -28,6 +47,7 @@ extern "C" void app_main() {
   spring::network::start();
   spring::wifi::start();
   spring::power::start();
+  xTaskCreate(interaction_task, "interaction", 3072, nullptr, 5, nullptr);
   spring::clock_app::register_app();
   spring::ota::mark_boot_valid();
   vTaskDelay(pdMS_TO_TICKS(1000));
